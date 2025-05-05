f1f055037b1b3792167d989b6139e9e217ff7a06
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-05 15:49:05 +0300

    try to add env copy

--- backend/cats/tests.py
+++ backend/cats/tests.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 
 
 class CatsAPITestCase(TestCase):
+
     def setUp(self):
         User = get_user_model()
         self.user = User.objects.create_user(username='auth_user')
@@ -13,6 +14,5 @@ class CatsAPITestCase(TestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
         response = self.client.get('/api/cats/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
